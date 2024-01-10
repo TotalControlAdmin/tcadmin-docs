3ba387ac64bbb824b0585a38be0655ff3ecef4f1
Adam Gliori <adam.gliori@hotmail.com>
2024-01-10 11:06:59 +0200

    Migrate docs to docusaurus to v3.1
    - fixed admonitions for v3 compatibility
    - fixed some MDX compatibility issues
    - using new docusaurus.config.js and merged existing settings into it
    - updated docusaurus to v3.1.0
    - updated @mdx-js/react to v3.0.0
    - updated clsx to v2.0.0
    - updated docusaurus-theme-search-typesense to v0.15.0-0
    - updated react to v18 and prism renderer to v2.3.0
    - added scrollbar support for other browsers

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,132 +1,141 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
 const lightLogo = "img/tcadmin-logo.png";
 const darkLogo = "img/TCAdmin_White.png";
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-
+/** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "TCAdmin Documentation",
-  tagline: "TCAdmin Documentation",
-  favicon: "img/cropped-tcadmin-favicon-32x32.png",
+  title: 'TCAdmin Documentation',
+  tagline: 'TCAdmin Documentation',
+  favicon: 'img/cropped-tcadmin-favicon-32x32.png',
 
-  url: "https://docs.tcadmin.com",
-  baseUrl: "/",
+  // Set the production url of your site here
+  url: 'https://docs.tcadmin.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
 
-  organizationName: "TotalControlAdmin",
-  projectName: "tcadmin-docs",
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'TotalControlAdmin', // Usually your GitHub org/user name.
+  projectName: 'tcadmin-docs', // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: './sidebars.js',
           showLastUpdateTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/TotalControlAdmin/tcadmin-docs/blob/master/",
+            'https://github.com/TotalControlAdmin/tcadmin-docs/blob/master/',
         },
-        blog: false, // Optional: disable the blog plugin
-        // ...
+        blog: false,
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: './src/css/custom.css',
         },
-      },
+      }),
     ],
   ],
-
-  themeConfig: {
-    image: "img/tcadmin-logo.png",
-    typesense: {
-      // Replace this with the name of your index/collection.
-      // It should match the "index_name" entry in the scraper's "config.json" file.
-      typesenseCollectionName: "tcadmin-docs",
-
-      typesenseServerConfig: {
-        nodes: [
+  
+  themes: ['docusaurus-theme-search-typesense'],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/tcadmin-logo.png',
+      typesense: {
+        // Replace this with the name of your index/collection.
+        // It should match the "index_name" entry in the scraper's "config.json" file.
+        typesenseCollectionName: "tcadmin-docs",
+  
+        typesenseServerConfig: {
+          nodes: [
+            {
+              host: "typesense.tcadmin.k8s.ovh.alexr03.dev",
+              port: 443,
+              protocol: "https",
+            },
+          ],
+          apiKey: "fzpKd6Esfg47mr8Nh9f4Km1Kbbi1OiFM",
+        },
+  
+        // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
+        typesenseSearchParameters: {},
+  
+        // Optional
+        contextualSearch: true,
+      },
+      navbar: {
+        title: '',
+        logo: {
+          alt: "TCAdmin Docs",
+          src: lightLogo,
+          srcDark: darkLogo,
+        },
+        items: [
           {
-            host: "typesense.tcadmin.k8s.ovh.alexr03.dev",
-            port: 443,
-            protocol: "https",
+            href: "https://github.com/TotalControlAdmin/tcadmin-docs",
+            label: "GitHub",
+            position: "right",
+          },
+          {
+            href: "https://discord.gg/ptN2pGf",
+            label: "Discord",
+            position: "right",
+          },
+          {
+            href: "https://community.tcadmin.com",
+            label: "Forums",
+            position: "right",
+          },
+          {
+            href: "https://www.tcadmin.com/",
+            label: "Main Website",
+            position: "right",
+          },
+          {
+            href: "https://clients.tcadmin.com/billing/clientarea.php",
+            label: "Billing",
+            position: "right",
           },
         ],
-        apiKey: "fzpKd6Esfg47mr8Nh9f4Km1Kbbi1OiFM",
       },
-
-      // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
-      typesenseSearchParameters: {},
-
-      // Optional
-      contextualSearch: true,
-    },
-    navbar: {
-      title: "",
-      logo: {
-        alt: "TCAdmin Docs",
-        src: lightLogo,
-        srcDark: darkLogo,
+      footer: {
+        style: "dark",
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} TCAdmin Built with Docusaurus.`,
       },
-      items: [
-        {
-          href: "https://github.com/TotalControlAdmin/tcadmin-docs",
-          label: "GitHub",
-          position: "right",
-        },
-        {
-          href: "https://discord.gg/ptN2pGf",
-          label: "Discord",
-          position: "right",
-        },
-        {
-          href: "https://community.tcadmin.com",
-          label: "Forums",
-          position: "right",
-        },
-        {
-          href: "https://www.tcadmin.com/",
-          label: "Main Website",
-          position: "right",
-        },
-        {
-          href: "https://clients.tcadmin.com/billing",
-          label: "Billing",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [],
-      copyright: `Copyright © ${new Date().getFullYear()} TCAdmin Built with Docusaurus.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-      additionalLanguages: ['php', 'cshtml', 'csharp'],
-    },
-    colorMode: {
-      respectPrefersColorScheme: true,
-    },
-  },
-  plugins: [
-    [
-      "docusaurus-theme-search-typesense",
-      {
-        typesenseSearchParameters: {
-          queryBy: "title,keywords,content",
-          numTypos: 2,
-        },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+        additionalLanguages: ['php', 'cshtml', 'csharp'],
       },
-    ],
-  ],
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+    }),
 };
 
-module.exports = config;
+export default config;
